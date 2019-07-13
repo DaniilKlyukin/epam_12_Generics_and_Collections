458deaec5179afb78a79d215ba7982603d2e652c
Daniil <anatoliikljukin@mail.ru>
2019-07-13 22:26:20 +0400

    Create TasksWorker.cs
    Solved 3rd task.

---
+++ TasksLibrary/TasksLibrary/TasksWorker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksLibrary
+{
+    public class TasksWorker
+    {
+        public IEnumerable<int> GetFibonacciNumber()
+        {
+            yield return 0;
+            yield return 1;
+
+            var prev = 0;
+            var current = 1;
+
+
+            while (true)
+            {
+                var newNumber = prev + current;
+                yield return newNumber;
+
+                prev = current;
+                current = newNumber;
+            }
+        }
+    }
+}
