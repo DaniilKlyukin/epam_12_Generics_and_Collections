6f298470967563348bf2dc42d09e5970913215ee
Daniil <anatoliikljukin@mail.ru>
2019-07-15 16:31:39 +0400

    Create Iterator.cs

---
+++ TasksLibrary/TasksLibrary/Iterator/Iterator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace TasksLibrary
+{
+    public abstract class Iterator<T> : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+
+        // Возвращает ключ текущего элемента
+        public abstract int Key();
+
+        // Возвращает текущий элемент.
+        public abstract T Current();
+
+        // Переходит к следующему элементу.
+        public abstract bool MoveNext();
+
+        // Перематывает Итератор к первому элементу.
+        public abstract void Reset();
+
+        public void Dispose()
+        {
+            this.Dispose();
+        }
+    }
+
+    public abstract class IteratorAggregate : IEnumerable
+    {
+        // Возвращает Iterator или другой IteratorAggregate для реализующего
+        // объекта.
+        public abstract IEnumerator GetEnumerator();
+    }
+}
