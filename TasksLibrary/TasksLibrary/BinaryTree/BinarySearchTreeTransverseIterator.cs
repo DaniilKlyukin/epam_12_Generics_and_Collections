37e4894f0274df26a2755c479c468144685b6879
Daniil <anatoliikljukin@mail.ru>
2019-07-22 17:50:22 +0400

    Create TreeIterator.cs
    Iterator for 7th task.

---
+++ TasksLibrary/TasksLibrary/BinaryTree/BinarySearchTreeTransverseIterator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksLibrary
+{
+    public class BinarySearchTreeTransverseIterator<T> : TreeIterator<T> where T : IComparable
+    {
+        BinarySearchTree<T> _collection;
+
+        // Хранит текущее положение обхода. У итератора может быть множество
+        // других полей для хранения состояния итерации, особенно когда он
+        // должен работать с определённым типом коллекции.
+        private Node<T> current;
+        private Stack<Node<T>> visited = new Stack<Node<T>>();
+
+        public BinarySearchTreeTransverseIterator(BinarySearchTree<T> collection)
+        {
+            this._collection = collection;
+
+            current = null;
+        }
+
+        public override Node<T> Current()
+        {
+            return current;
+        }
+
+        public override bool MoveNext()
+        {
+            if (current == null)
+            {
+                Reset();
+                visited.Push(current);
+                return true;
+            }
+
+
+            if (current.Left != null && !visited.Contains(current.Left))
+            {
+                current = current.Left;
+                visited.Push(current);
+                return true;
+            }
+            if (current.Right != null && !visited.Contains(current.Right))
+            {
+                current = current.Right;
+                visited.Push(current);
+                return true;
+            }
+            else if (current.Parent != null)
+            {
+                current = current.Parent;
+                if (!visited.Contains(current))
+                {
+                    visited.Push(current);
+                    return true;
+                }
+                else
+                {
+                    while(true)
+                    {
+                        current = current.Parent;
+                        if (!visited.Contains(current))
+                            break;
+                    }
+                    if (current == null)
+                        return false;
+                    else
+                    {
+                        visited.Push(current);
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override void Reset()
+        {
+            visited = new Stack<Node<T>>();
+            current = _collection.root;
+            while (true)
+            {
+                if (current.Left != null)
+                    current = current.Left;
+                else
+                    break;
+            }
+        }
+    }
+}
---
+++ TasksLibrary/TasksLibrary/BinaryTree/TreeIterator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace TasksLibrary
+{
+    public abstract class TreeIterator<T> : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+
+        // Возвращает текущий элемент.
+        public abstract Node<T> Current();
+
+        // Переходит к следующему элементу.
+        public abstract bool MoveNext();
+
+        // Перематывает Итератор к первому элементу.
+        public abstract void Reset();
+
+        public void Dispose()
+        {
+            this.Dispose();
+        }
+    }
+}
