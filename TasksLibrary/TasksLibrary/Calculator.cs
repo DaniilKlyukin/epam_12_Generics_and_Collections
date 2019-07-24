de23cd7d261e67bd5b1f0487747cde882f4ddfc6
Daniil <anatoliikljukin@mail.ru>
2019-07-24 19:38:57 +0400

    Create Calculator.cs
    Solved 8th task (Reverse Polish notation calculator).

---
+++ TasksLibrary/TasksLibrary/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TasksLibrary
+{
+    public class Calculator
+    {
+        static Dictionary<string, Func<double, double, double>> operations = new Dictionary<string, Func<double, double, double>>
+        {
+            { "+", (y, x) => x + y },
+            { "-", (y, x) => x - y },
+            { "*", (y, x) => x * y },
+            { "/", (y, x) => x / y },
+        };
+
+        public double Calculate(string expression)
+        {
+            var dataArray = expression.Split(' ');
+
+            var stack = new Stack<double>();
+            foreach (var e in dataArray)
+            {
+                var num = 0;
+                var isParsed = int.TryParse(e, out num);
+
+                if (isParsed)
+                    stack.Push(num);
+                else if (operations.ContainsKey(e))
+                    stack.Push(operations[e](stack.Pop(), stack.Pop()));
+                else
+                    throw new ArgumentException();
+            }
+
+            return stack.Pop();
+        }
+    }
+}
